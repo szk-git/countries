9aa3bda0b07304700e277097994cd817a7820a23
Kristóf Szabó <aisdos007@gmail.com>
2023-08-29 00:15:41 +0200

    [NO JIRA] Add AOP logging

--- src/main/java/com/countries/CountriesApplication.java
+++ src/main/java/com/countries/CountriesApplication.java
@@ -2,10 +2,12 @@ package com.countries;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class CountriesApplication {
 
 	public static void main(String[] args) {
